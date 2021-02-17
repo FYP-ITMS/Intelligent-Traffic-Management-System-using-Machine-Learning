2ae7d608fb24f004272a9c75eaa0fb21777e0de5
Ashwin <47941624+ashwiii@users.noreply.github.com>
2021-02-17 13:34:56 +0530

    Update signal_switching.py

--- util/signal_switching.py
+++ util/signal_switching.py
@@ -1,10 +1,10 @@
 import time
-def countdown(seconds,denser_lane):
-	print("Opening Lane : {} for 20 seconds".format(str(denser_lane)))
-	while seconds:
-		mins,secs = divmod(t,60)
-		timer = '{:02d}:{:02d}'.format(mins, secs)
-		print(timer,end="\r")
-		time.sleep(1)
-		seconds-=1
-	print("Closing Lane : {}".format(str(denser_lane)))
+def countdown(seconds, denser_lane):
+    print("Opening Lane - {} for 20 seconds".format(str(denser_lane)))
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        print(".", end="")
+        time.sleep(1)
+        seconds -= 1
+    print("\nClosing Lane - {}".format(str(denser_lane)))
+
