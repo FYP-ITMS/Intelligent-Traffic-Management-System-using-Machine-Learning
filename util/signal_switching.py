d36517534b72832f23f9af9236c1278e6848ac36
Ashwin <47941624+ashwiii@users.noreply.github.com>
2021-02-17 13:58:18 +0530

    Update signal_switching.py

--- util/signal_switching.py
+++ util/signal_switching.py
@@ -1,9 +1,10 @@
 import time
+import emoji
 def countdown(seconds, denser_lane):
     print('\033[92m' +"Opening Lane - {} for 20 seconds".format(str(denser_lane)))
     while seconds:
         mins, secs = divmod(seconds, 60)
-        print(".", end="")
+        print('\033[90m'+".", end="")
         time.sleep(1)
         seconds -= 1
-    print('\033[91m' + "\nClosing Lane - {}".format(str(denser_lane)))
+    print(emoji.emojize(":red_circle:")+'\033[91m' + "\nClosing Lane - {}".format(str(denser_lane)))
