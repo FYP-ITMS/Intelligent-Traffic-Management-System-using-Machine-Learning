0731ab1247ab5eadbdbe9bd1db051ab1951dab32
Ashwin <47941624+ashwiii@users.noreply.github.com>
2021-02-17 10:45:35 +0530

    module-4-development-change-1

---
+++ util/signal-switching.py
@@ -0,0 +1,263 @@
+import IPython
+from google.colab import output
+display(IPython.display.HTML('''<div class="container">
+	<div class="circle red" color="red"></div>
+	<div class="circle" color="yellow"></div>
+	<div class="circle" color="green"></div>
+
+
+
+<div class="floating-text">
+	Intelligent Traffic Management System </div>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+
+* {
+	box-sizing: border-box;
+}
+
+body {
+	background-color: #1abc9c;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
+	margin: 0;
+}
+
+.container {
+	background-color: #2c3e50;
+	border-radius: 50px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+	padding: 15px 0;
+	height: 200px;
+	width: 70px;
+}
+
+.circle {
+	background-color: rgba(0, 0, 0, 0.3);
+	border-radius: 100%;
+	position: relative;
+	height: 40px;
+	width: 40px;
+}
+
+.circle::after {
+	border-right: 4px solid rgba(255, 255, 255, 0.6);
+	border-radius: 100%;
+	content: ' ';
+	position: absolute;
+	top: 5px;
+	left: 0px;
+	width: 30px;
+	height: 30px;
+}
+
+.circle.red {
+	background-color: #c0392b;
+	box-shadow: 0 0 20px 5px #c0392b;
+}
+
+.circle.yellow {
+	background-color: #f1c40f;
+	box-shadow: 0 0 20px 5px #f1c40f;
+}
+
+.circle.green {
+	background-color: #2ecc71;
+	box-shadow: 0 0 20px 5px #2ecc71;
+}
+
+
+
+
+
+
+/* SOCIAL PANEL CSS */
+.social-panel-container {
+	position: fixed;
+	right: 0;
+	bottom: 80px;
+	transform: translateX(100%);
+	transition: transform 0.4s ease-in-out;
+}
+
+.social-panel-container.visible {
+	transform: translateX(-10px);
+}
+
+.social-panel {	
+	background-color: #fff;
+	border-radius: 16px;
+	box-shadow: 0 16px 31px -17px rgba(0,31,97,0.6);
+	border: 5px solid #001F61;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	font-family: 'Muli';
+	position: relative;
+	height: 169px;	
+	width: 370px;
+	max-width: calc(100% - 10px);
+}
+
+.social-panel button.close-btn {
+	border: 0;
+	color: #97A5CE;
+	cursor: pointer;
+	font-size: 20px;
+	position: absolute;
+	top: 5px;
+	right: 5px;
+}
+
+.social-panel button.close-btn:focus {
+	outline: none;
+}
+
+.social-panel p {
+	background-color: #001F61;
+	border-radius: 0 0 10px 10px;
+	color: #fff;
+	font-size: 14px;
+	line-height: 18px;
+	padding: 2px 17px 6px;
+	position: absolute;
+	top: 0;
+	left: 50%;
+	margin: 0;
+	transform: translateX(-50%);
+	text-align: center;
+	width: 235px;
+}
+
+.social-panel p i {
+	margin: 0 5px;
+}
+
+.social-panel p a {
+	color: #FF7500;
+	text-decoration: none;
+}
+
+.social-panel h4 {
+	margin: 20px 0;
+	color: #97A5CE;	
+	font-family: 'Muli';	
+	font-size: 14px;	
+	line-height: 18px;
+	text-transform: uppercase;
+}
+
+.social-panel ul {
+	display: flex;
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+}
+
+.social-panel ul li {
+	margin: 0 10px;
+}
+
+.social-panel ul li a {
+	border: 1px solid #DCE1F2;
+	border-radius: 50%;
+	color: #001F61;
+	font-size: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 50px;
+	width: 50px;
+	text-decoration: none;
+}
+
+.social-panel ul li a:hover {
+	border-color: #FF6A00;
+	box-shadow: 0 9px 12px -9px #FF6A00;
+}
+
+.floating-btn {
+	border-radius: 26.5px;
+	background-color: #001F61;
+	border: 1px solid #001F61;
+	box-shadow: 0 16px 22px -17px #03153B;
+	color: #fff;
+	cursor: pointer;
+	font-size: 16px;
+	line-height: 20px;
+	padding: 12px 20px;
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	z-index: 999;
+}
+
+.floating-btn:hover {
+	background-color: #ffffff;
+	color: #001F61;
+}
+
+.floating-btn:focus {
+	outline: none;
+}
+
+.floating-text {
+	background-color: #001F61;
+	border-radius: 10px 10px 0 0;
+	color: #fff;
+	font-family: 'Muli';
+	padding: 7px 15px;
+	position: fixed;
+	bottom: 0;
+	left: 50%;
+	transform: translateX(-50%);
+	text-align: center;
+	z-index: 998;
+}
+
+.floating-text a {
+	color: #FF7500;
+	text-decoration: none;
+}
+
+@media screen and (max-width: 480px) {
+
+	.social-panel-container.visible {
+		transform: translateX(0px);
+	}
+	
+	.floating-btn {
+		right: 10px;
+	}
+}
+</style>
+<script>
+const circles = document.querySelectorAll('.circle')
+let activeLight = 0;
+
+setInterval(() => {
+	changeLight();
+}, 1000);
+
+function changeLight() {
+	circles[activeLight].className = 'circle';
+	activeLight++;
+	
+	if(activeLight > 2) {
+		activeLight = 0;
+	}
+	
+	const currentLight = circles[activeLight]
+	
+	currentLight.classList.add(currentLight.getAttribute('color'));
+}
+
+</script>
+    '''))
